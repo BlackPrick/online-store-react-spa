c53318cc520c4437df57b60e5d84177ac025cb1f
BlackPrick <122222996+BlackPrick@users.noreply.github.com>
2023-03-29 20:29:43 +0100

    Updates

---
+++ precache-manifest.7339f50dae6756a53d9f12756ffd8335.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "35adffc93a6ddcfb32ad",
+    "url": "/online-store-react-spa/static/css/main.d3817c3a.chunk.css"
+  },
+  {
+    "revision": "35adffc93a6ddcfb32ad",
+    "url": "/online-store-react-spa/static/js/main.35adffc9.chunk.js"
+  },
+  {
+    "revision": "ed197c0f677114f68a6f",
+    "url": "/online-store-react-spa/static/js/1.ed197c0f.chunk.js"
+  },
+  {
+    "revision": "e8aa2d9dfcf98c03642e",
+    "url": "/online-store-react-spa/static/js/2.e8aa2d9d.chunk.js"
+  },
+  {
+    "revision": "e1f70bfb4cfe526b7cd7",
+    "url": "/online-store-react-spa/static/js/runtime~main.e1f70bfb.js"
+  },
+  {
+    "revision": "b887f73c03e8246046bc4688a87b526d",
+    "url": "/online-store-react-spa/static/media/Blogger-Light.b887f73c.otf"
+  },
+  {
+    "revision": "9b6bdd2608ad69f1e4f379fadcd6aec5",
+    "url": "/online-store-react-spa/static/media/Modak.9b6bdd26.ttf"
+  },
+  {
+    "revision": "09a3b1213a434385ec9af5bbc8879fd7",
+    "url": "/online-store-react-spa/index.html"
+  }
+];
