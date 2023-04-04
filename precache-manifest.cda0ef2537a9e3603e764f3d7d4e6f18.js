481a8c89b776dd3e60dc145203ecb223024259ac
BlackPrick <122222996+BlackPrick@users.noreply.github.com>
2023-04-04 17:54:00 +0100

    Updates

--- precache-manifest.cda0ef2537a9e3603e764f3d7d4e6f18.js
+++ precache-manifest.cda0ef2537a9e3603e764f3d7d4e6f18.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6a9c8df6f5cd76b402f5",
+    "revision": "512b605165e0f5e4b1ed",
     "url": "/online-store-react-spa/static/css/main.f0d821e1.chunk.css"
   },
   {
-    "revision": "6a9c8df6f5cd76b402f5",
-    "url": "/online-store-react-spa/static/js/main.6a9c8df6.chunk.js"
+    "revision": "512b605165e0f5e4b1ed",
+    "url": "/online-store-react-spa/static/js/main.512b6051.chunk.js"
   },
   {
     "revision": "ed197c0f677114f68a6f",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/online-store-react-spa/static/media/Modak.9b6bdd26.ttf"
   },
   {
-    "revision": "3d5ba3fe8e3771910b6e091095947d6e",
+    "revision": "70e51d47e4af31c9b483d8a2e5358490",
     "url": "/online-store-react-spa/index.html"
   }
 ];
